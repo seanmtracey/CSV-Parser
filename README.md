5577705b69cbbdf1b8479a2cb751382568e5ae86
seanmtracey <seanmtracey@gmail.com>
2014-03-08 10:15:36 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ CSV-Parser
 ==========
 
 A quick Python script to convert CSV files to JSON
+
+##Usage
+
+CLI:
+python parse.py [CSV FILE PATH] 
+
+The file will output a JSON file in the same place as the Python file.
---
+++ parse.py
@@ -0,0 +1,44 @@
+import sys, os, json
+
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename", help="write report to FILE", metavar="FILE")
+
+(options, args) = parser.parse_args()
+
+def convertCSV(fileName):
+
+	print "Parsing " + fileName
+
+	f = open(fileName, 'rb')
+
+	data = []
+
+	fileKeys = f.readline().lstrip().rstrip().split(',')
+
+	for line in f:
+		
+		thisLine = line.lstrip().rstrip().split(',')
+
+		if thisLine[0] != '\x00' and thisLine[0] != '':
+
+			thisObject = {}
+
+			for idx, key in enumerate(fileKeys):
+				
+				thisObject[key] = thisLine[idx]
+
+			#print thisObject
+
+			data.append(thisObject);
+
+	nf = open(fileName.replace('.csv', '.json'), 'w')
+	nf.write(json.dumps(data))
+
+	print "Parsing complete"
+
+
+#convertCSV('/Users/seantracey/Downloads/ndpb-high-earners.csv')
+
+convertCSV(args[0])
